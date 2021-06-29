bcf5d3016667fbfe52d6ad034ba4253ab547f983
Ian Shaw <ianshaw93@gmail.com>
2021-06-29 08:23:17 +0100

    changed location name to Southwark for clarity

--- static/js.js
+++ static/js.js
@@ -1,14 +1,9 @@
-// This example displays a marker at the center of Australia.
-// When the user clicks the marker, an info window opens.
-
-// This example displays a marker at the center of Australia.
-// When the user clicks the marker, an info window opens.
 var test_name = 42;
 function initMap(test_name=45) {
-  const uluru = { lat: 51.5028, lng: 0.0877 };
+  const southwark = { lat: 51.5028, lng: 0.0877 };
   const map = new google.maps.Map(document.getElementById("map"), {
     zoom: 4,
-    center: uluru,
+    center: southwark,
   });
   const contentString =
     'The wind speed is ${test_name}';
@@ -16,7 +11,7 @@ function initMap(test_name=45) {
     content: contentString,
   });
   const marker = new google.maps.Marker({
-    position: uluru,
+    position: southwark,
     map,
     title: "Uluru (Ayers Rock)",
   });
