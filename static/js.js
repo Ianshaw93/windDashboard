d67528e16019fd14bbdef6e8a164951637e9e633
Ian Shaw <ianshaw93@gmail.com>
2021-06-29 08:22:06 +0100

    passed in dummy wind speed in description box on Map for Southwark

--- static/js.js
+++ static/js.js
@@ -11,20 +11,7 @@ function initMap(test_name=45) {
     center: uluru,
   });
   const contentString =
-    '<div id="content">' +
-    '<div id="siteNotice">' +
-    "</div>" +
-    '<h1 id="firstHeading" class="firstHeading"> ' + test_name + ' </h1>' +
-    '<div id="bodyContent">' +
-    "<p><b>Uluru</b>," +
-    "sandstone rock formation in the southern part of the " +
-    "Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) " +
-    "Heritage Site.</p>" +
-    '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">' +
-    "https://en.wikipedia.org/w/index.php?title=Uluru</a> " +
-    "(last visited June 22, 2009).</p>" +
-    "</div>" +
-    "</div>";
+    'The wind speed is ${test_name}';
   const infowindow = new google.maps.InfoWindow({
     content: contentString,
   });
